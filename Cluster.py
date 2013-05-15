7c3dca3f7d873b59a7a907676cbafe8da1ae5eee
Sambit <04msambit@gmail.com>
2013-05-14 21:57:09 -0500

    Adding files

---
+++ Cluster.py
@@ -0,0 +1,471 @@
+import sys
+import json
+import requests0 as requests
+import simplejson
+import difflib
+import re
+import collections
+import math
+import numpy
+import operator
+import random
+
+
+idf_dictionary=collections.defaultdict(float)
+docClass=collections.defaultdict(dict)
+document_list=collections.defaultdict(dict)
+title_list = collections.defaultdict(dict)
+cluster = [[]]
+mini_cluster = []
+
+def document_parsing(r):
+
+    d = collections.defaultdict(float)
+    set_list1 = set(r);
+           
+    for word in set_list1:
+        if idf_dictionary.has_key(word):
+           idf_dictionary[word]+=1;
+        else:
+           idf_dictionary[word]=1;
+   
+    # We will create a term frequency dictionary here
+
+    for wrd in r:
+        if  wrd not in d:
+            d[wrd]=1
+        else:
+            d[wrd]+=1
+
+    # Now we will convert it to log form
+
+    for ky in d:
+        value = d[ky]
+        d[ky]=1+float(math.log(value,2))
+
+      
+    return d
+
+def bing_search_clustering(fileName):
+ 
+    d = collections.defaultdict(float)
+    list=[]
+    
+    count =0
+    docId = 0
+
+    for i in range(30):
+ 	docClass[i]="texas longhorns"
+    
+    for i in range(30,60):
+ 	docClass[i]="texas aggies"
+    
+    for i in range(60,90):
+ 	docClass[i]="dallas cowboys"
+    
+    for i in range(90,120):
+ 	docClass[i]="dallas mavericks"
+    
+    for i in range(120,150):
+ 	docClass[i]="duke blue devils"
+
+    count = 0
+    lines = [line.strip() for line in open(fileName)]
+      
+    for line in lines:
+        news=json.loads(line)
+        text=news['Title']+' '+news['Description']
+        wordlist=word_preprocessing(text)       
+        document_list[docId] = document_parsing(wordlist)
+        title_list[docId] = news['Title'] 
+        docId+=1
+        count+=1
+
+    
+        
+    
+    for key in idf_dictionary:
+        val = len(document_list)/float(idf_dictionary[key])
+        idf_dictionary[key]=math.log(val,2)
+
+
+    for i in range(0,len(document_list)):
+        for key in document_list[i]:
+            if key in idf_dictionary:
+               idf_value = idf_dictionary[key]    # We are getting the idf value for that term
+            else:
+               idf_value = 0
+            
+            document_list[i][key] = float(document_list[i][key]) * float(idf_value)
+            
+
+    # We will normalize the dicument list now
+
+    value_g=0
+    sum_element=0
+
+    for i in range(0,len(document_list)):
+        for key in document_list[i]:
+            value_g = (document_list[i][key] * document_list[i][key])
+            sum_element+=value_g
+        sum_element=math.sqrt(sum_element)
+        for every_key in document_list[i]:
+            document_list[i][every_key] = float( document_list[i][every_key] ) / float(sum_element)
+
+    flag_control=1
+
+    while(flag_control):   
+
+       choice = raw_input('\nInput your choice: 1- Input Own Cluster Number(k value) 2 : Use Default k value\n')  
+       ch = int(choice)   
+
+       if ( ch == 1):
+      
+          inpt = raw_input('\nInput the number of clusters\n') 
+          input_range = int(inpt)
+          flag_control=0
+
+       elif ( ch == 2 ):
+            input_range = 5         
+            flag_control=0
+       else:
+            print 'Input Correct Choice i.e. 1 or 2'
+
+    
+
+    cluster_id=[]
+    cluster_id_new=[]
+    id_list=[]
+    cluster=[]
+    
+
+    # Forming the initial clusters
+
+    for n in range(0,input_range):
+        rand_val = random.randint(0,149)
+        #mini_cluster.append(document_list[rand_val])
+        id_list.append(rand_val)
+        cluster_id.append(id_list)
+        id_list=[]   
+        cluster_id_new.append([])     
+
+
+    # K-Means Algorithm
+  
+    mean = []
+    comp1=[]
+    comp2=[]
+    
+        
+    iteration_max = 15
+
+    convergence_count = 0
+    flag = 1
+
+   
+    while(flag):
+
+         # Finding the Centroid 
+         mean=[]
+         
+                        
+         for i in range(0,len(cluster_id)):
+             
+             if len(cluster_id[i]) !=0:
+                mean.append(find_mean(cluster_id[i]))      
+                                
+                             
+         for values in range(0,len(document_list)):
+        	ind=give_min_distance(document_list[values],mean)
+             
+                cluster_id_new[ind].append(values)
+
+                
+
+         flg = 1
+
+         for rng in range(0,len(cluster_id)):
+             comp1=cluster_id[rng]
+             comp2=cluster_id_new[rng]
+             if comp1 != comp2:
+                flg = 0
+         
+         if(flg == 0):
+           
+           #Reform the Cluster
+           
+           cluster_id=[]
+ 
+           for h in range(0,len(cluster_id_new)):
+               id_list=[]
+               id_list=cluster_id_new[h]
+               
+               cluster_id.append(id_list)
+
+           cluster_id_new=[]         
+           
+           for h in range(0,len(cluster_id)):
+               cluster_id_new.append([])
+           
+
+         
+         if(flg == 1):
+              flag = 0
+  
+         convergence_count+=1
+
+         if(convergence_count > iteration_max):
+           flag = 0;
+     
+    print 'The Clusters have converged and we will print the clusters now :\n'
+
+       
+    for l in range(0,len(cluster_id)):
+        print 'Cluster ',
+        print  l
+        for val in range(0,len(cluster_id[l])):
+            print docClass[cluster_id[l][val]],
+            print ': ',
+            print title_list[cluster_id[l][val]]
+
+
+    
+    print '\nThe Algorithm Converges in ',
+    print convergence_count,
+    print ' iterations'
+
+          
+
+    # Calculate RSS
+
+    rss=0
+    dist = 0
+
+    for lst_clus in range(0,len(cluster_id)):
+        centroid_vect=find_mean(cluster_id[lst_clus])
+        for every_element in range(0,len(cluster_id[lst_clus])):
+            dist=distance(centroid_vect,document_list[cluster_id[lst_clus][every_element]])
+            dist = dist * dist
+            rss+=dist
+ 
+
+    print '\nThe RSS for k value ',
+    print input_range,
+    print ' is ',
+    print rss
+
+    # Calculate Purity
+    clust_1=0
+    clust_2=0
+    clust_3=0
+    clust_4=0
+    clust_5=0
+
+    total_sum =0
+
+    for each_cluster in range(0,len(cluster_id)):
+        for id in range(0,len(cluster_id[each_cluster])):
+            if(0<= cluster_id[each_cluster][id] <30):
+              clust_1+=1
+            elif(30<= cluster_id[each_cluster][id] <60):
+              clust_2+=1
+            elif(60<= cluster_id[each_cluster][id] < 90):
+              clust_3+=1
+            elif(90<= cluster_id[each_cluster][id] < 120):
+              clust_4+=1
+            elif(120<= cluster_id[each_cluster][id] < 150):
+              clust_5+=1
+
+        total_sum+=max(clust_1,clust_2,clust_3,clust_4,clust_5)
+	clust_1=0
+        clust_2=0
+        clust_3=0
+        clust_4=0
+        clust_5=0
+
+    purity = float(total_sum) / len(document_list)
+
+    print '\nThe Purity for k value ',
+    print input_range,
+    print ' is ',
+    print purity
+
+    
+
+    # Calculate the Rand Index
+    
+    tp=0
+    clust_1=0
+    clust_2=0
+    clust_3=0
+    clust_4=0
+    clust_5=0
+ 
+
+    for each_cluster in range(0,len(cluster_id)):
+        clust_1=0
+        clust_2=0
+        clust_3=0
+        clust_4=0
+        clust_5=0
+        for idl in range(0,len(cluster_id[each_cluster])):
+            if(0<= cluster_id[each_cluster][idl] <30):
+              clust_1+=1
+            elif(30<= cluster_id[each_cluster][idl] <60):
+              clust_2+=1
+            elif(60<= cluster_id[each_cluster][idl] < 90):
+              clust_3+=1
+            elif(90<= cluster_id[each_cluster][idl] < 120):
+              clust_4+=1
+            elif(120<= cluster_id[each_cluster][idl] < 150):
+              clust_5+=1
+        tp+= combination(clust_1) + combination(clust_2) + combination(clust_3) + combination(clust_4)+ combination(clust_5)
+
+    #print tp    
+
+    tn = 0
+
+    clust_1=0
+    clust_2=0
+    clust_3=0
+    clust_4=0
+    clust_5=0
+
+    clust=[]
+       
+
+    for each_cluster in range(0,len(cluster_id)):
+        
+        clust_1=0
+        clust_2=0
+        clust_3=0
+        clust_4=0
+        clust_5=0
+
+        for id in range(0,len(cluster_id[each_cluster])):
+            if(0<= cluster_id[each_cluster][id] <30):
+              clust_1+=1
+            elif(30<= cluster_id[each_cluster][id] <60):
+              clust_2+=1
+            elif(60<= cluster_id[each_cluster][id] < 90):
+              clust_3+=1
+            elif(90<= cluster_id[each_cluster][id] < 120):
+              clust_4+=1
+            elif(120<= cluster_id[each_cluster][id] < 150):
+              clust_5+=1
+        lst_val=[]
+        lst_val=[clust_1,clust_2,clust_3,clust_4,clust_5]
+        clust.append(lst_val) 
+
+    for val in range(0,len(clust)):
+        for j in range(0,len(clust[val])):
+            for k in range(val+1,len(clust)):
+                iter_clust = clust[k]
+                for l in range(0,len(iter_clust)):
+                      if( l != j):
+                        tn+= clust[val][j] * iter_clust[l]
+                  
+    numerator = tn + tp
+
+    denominator = combination(len(document_list))
+
+    
+    rand_index = float(numerator) / denominator
+
+    print '\nThe RI[Rand_Index] value for k value ',
+    print input_range,
+    print ' is ',
+    print rand_index
+    print '\n'
+
+    return 
+
+def combination(val):
+
+    ret_val = 0
+
+    if val == 0 :
+       ret_val = 0
+    elif val == 1:
+       ret_val = 0
+    elif val == 2:
+       ret_val = 1
+    else:
+        ret_val = float( val * (val -1 ) ) / 2
+
+
+    return ret_val
+
+
+def distance(vect,compare):
+    sum=0 
+    for key in compare:
+            if key in vect:
+               sum+=float( (compare[key] - vect[key]) * (compare[key] - vect[key]) )
+            else:
+               sum+=float( compare[key] * compare[key] )
+
+    dist = math.sqrt(sum)
+     
+    return dist
+
+
+
+def find_mean(lst):
+   
+     
+    mn = collections.defaultdict(float)
+    vect = collections.defaultdict(float)
+    
+
+    for i in range(0,len(lst)):
+        
+        vect = document_list[lst[i]]
+        for each_key in vect:
+            if each_key not in mn:
+                   mn[each_key] = vect[each_key]
+            else:
+                    #mn[key]+=iter_lst[i][key]
+                    mn[each_key]+=vect[each_key]
+
+    for val in (mn):
+        mn[val] = float( mn[val]/float(len(lst)) )
+
+
+    return mn
+
+
+def word_preprocessing(word):
+ return re.findall(r"[\w]+", word.lower())
+
+
+def give_min_distance(compare,mn_list):
+    
+    sum=0   
+    dist = 0 
+    lst = []
+
+    for val in mn_list:
+        sum = 0
+        for key in compare:
+            if key in val:
+               sum+=float( (compare[key] - val[key]) * (compare[key] - val[key]) ) 
+            else:
+               sum+=float( compare[key] * compare[key] )
+
+        dist = math.sqrt(sum)
+        lst.append(dist)
+
+    minimum_value = min(lst)
+    index = lst.index(minimum_value)    
+
+ 
+
+    return index
+
+def main():
+    
+    bing_search_clustering(sys.argv[1]);
+       
+if __name__ == '__main__':
+  main()
---
+++ Composite_classifier.py
@@ -0,0 +1,440 @@
+import sys
+import json
+import requests0 as requests
+import simplejson
+import difflib
+import re
+import collections
+import math
+import numpy
+import operator
+import random
+
+
+term_dict_business=collections.defaultdict(dict)
+term_dict_politics=collections.defaultdict(dict)
+term_dict_entertainment=collections.defaultdict(dict)
+
+term_dict_business_frequency=collections.defaultdict(dict)
+term_dict_politics_frequency=collections.defaultdict(dict)
+term_dict_entertainment_frequency=collections.defaultdict(dict)
+
+
+document_list=collections.defaultdict()
+
+probability_entertainment=0.0
+probability_business = 0.0
+probability_politics = 0.0
+
+total_terms_entertainment_class=0
+total_terms_business_class=0
+total_terms_politics_class=0
+
+class_business=[]
+class_entertainment=[]
+class_politics=[]
+
+
+tp_business=0
+tp_politics=0
+tp_entertainment=0
+
+fp_business=0
+fp_politics=0
+fp_entertainment=0
+
+
+fn_business=0
+fn_politics=0
+fn_entertainment=0
+
+
+def naive_bays_classification(fileName1,fileName2,fileName3,fileName4,fileName5,fileName6):
+ 
+    d_sorted_by_value={}        
+    count =0
+    docId =0
+    
+    global total_terms_entertainment_class
+    global total_terms_business_class
+    global total_terms_politics_class
+
+    global probability_entertainment
+    global probability_business
+    global probability_politics
+
+    
+    entertainment_class_length=0
+    business_class_length=0
+    politics_class_length=0
+
+    global_document_count=0
+
+    lines = [line.strip() for line in open(fileName1)]
+      
+    for line in lines:
+        news=json.loads(line)
+        text=news['Title']+' '+news['Description']
+        wordlist=word_preprocessing(text)
+        
+        total_terms_entertainment_class += len(wordlist)   
+
+        for each_term in wordlist:
+            if each_term not in term_dict_entertainment:
+               term_dict_entertainment[each_term]=1
+            else:
+               term_dict_entertainment[each_term]+=1
+             
+        entertainment_class_length+=1
+        global_document_count+=1
+        wordlist=[]
+
+    # Frequency of the Entertainment
+
+    value_list=[]
+    for key in term_dict_entertainment:
+        value_list.append(term_dict_entertainment[key])
+
+    value_list.sort(reverse=True)
+
+    length= int ( 0.85 * len(value_list))
+    counter = 0
+    term_dict_entertainment_frequency={}
+    for i in range(0,length):
+        search_value=value_list[i]
+        for keys in term_dict_entertainment:
+            if ( term_dict_entertainment[keys] == search_value ):
+                if ( counter < length):
+                   if keys not in term_dict_entertainment_frequency:
+                      term_dict_entertainment_frequency[keys] = search_value
+                      counter+=1
+
+        
+    lines = [line.strip() for line in open(fileName2)]
+
+    for line in lines:
+        news=json.loads(line)
+        text=news['Title']+' '+news['Description']
+        wordlist=word_preprocessing(text)
+
+        total_terms_business_class+=len(wordlist)
+
+        for each_term in wordlist:
+            if each_term not in term_dict_business:
+               term_dict_business[each_term]=1
+            else:
+               term_dict_business[each_term]+=1
+
+        business_class_length+=1
+        global_document_count+=1
+        wordlist=[]
+
+    # Frequency Dictionary for Feature Selection
+    
+    value_list=[]
+    for key in term_dict_business:
+        value_list.append(term_dict_business[key])
+
+    value_list.sort(reverse=True)
+
+    length= int ( 0.85 * len(value_list))
+    counter=0
+    term_dict_business_frequency={}
+    for i in range(0,length):
+        search_value=value_list[i]
+        for keys in term_dict_business:
+            if ( term_dict_business[keys] == search_value ):
+                if (counter < length):
+                   if keys not in term_dict_business_frequency:
+                      term_dict_business_frequency[keys] = search_value
+	              counter+=1
+    
+    
+
+    
+    lines = [line.strip() for line in open(fileName3)]
+
+    for line in lines:
+        news=json.loads(line)
+        text=news['Title']+' '+news['Description']
+        wordlist=word_preprocessing(text)
+
+        total_terms_politics_class += len(wordlist)
+
+        for each_term in wordlist:
+            if each_term not in term_dict_politics:
+               term_dict_politics[each_term]=1
+            else:
+               term_dict_politics[each_term]+=1
+
+        politics_class_length+=1
+        global_document_count+=1
+        wordlist=[]
+
+    value_list=[]
+    for key in term_dict_politics:
+        value_list.append(term_dict_politics[key])
+
+    
+    value_list.sort(reverse=True)
+
+    length= int ( 0.85 * len(value_list))
+    counter = 0
+    term_dict_politics_frequency={}
+    for i in range(0,length):
+        search_value=value_list[i]
+        for keys in term_dict_politics:
+            if ( term_dict_politics[keys] == search_value ):
+                if ( counter < length ):
+                   if keys not in term_dict_politics_frequency:
+                   	term_dict_politics_frequency[keys] = search_value
+
+        
+    probability_entertainment = float( entertainment_class_length) / global_document_count 
+    probability_business = float( business_class_length) / global_document_count    
+    probability_politics = float( politics_class_length) / global_document_count
+  
+
+       
+
+    lines = [line.strip() for line in open(fileName4)]
+
+    for line in lines:
+        news=json.loads(line)
+        text=news['Title']+' '+news['Description']
+        title=news['Title']
+        wordlist=word_preprocessing(text)
+        
+        calculate_class(wordlist,title,0)
+            
+
+    lines = [line.strip() for line in open(fileName5)]
+
+    for line in lines:
+        news=json.loads(line)
+        text=news['Title']+' '+news['Description']
+        title=news['Title']
+        wordlist=word_preprocessing(text)
+
+        calculate_class(wordlist,title,1)
+  
+    lines = [line.strip() for line in open(fileName6)]
+
+    for line in lines:
+        news=json.loads(line)
+        text=news['Title']+' '+news['Description']
+        title=news['Title']
+        wordlist=word_preprocessing(text)
+
+        calculate_class(wordlist,title,2)
+
+    
+    tp_aggr = tp_business + tp_politics + tp_entertainment
+    fp_aggr = fp_business + fp_politics + fp_entertainment
+    fn_aggr = fn_business + fn_politics + fn_entertainment
+
+    p_aggr = float(tp_aggr) / ( tp_aggr + fp_aggr)
+    r_aggr = float(tp_aggr) / ( tp_aggr + fn_aggr)
+
+    f_aggr = float( 2 * float(p_aggr * r_aggr)) / (p_aggr + r_aggr)
+
+    print 'Entertainment\n'
+    for ll in range(0,len(class_entertainment)):
+        print class_entertainment[ll][0],
+        print ' ',
+        print class_entertainment[ll][1]
+
+
+    print '\nBusiness\n'
+    for ll in range(0,len(class_business)):
+        print class_business[ll][0],
+        print ' ',
+        print class_business[ll][1]
+
+    print '\nPolitics\n'
+    for ll in range(0,len(class_politics)):
+        print class_politics[ll][0],
+        print ' ',
+        print class_politics[ll][1]
+
+
+
+    print '\nEntertainment Class'
+    print 'True Positive',
+    print tp_entertainment,
+    print 'False Positive',
+    print fp_entertainment,
+    print 'False Negative',
+    print fn_entertainment,
+    print 'True Negative',
+    print ( len(class_entertainment) + len(class_business) + len(class_politics) - tp_entertainment - fp_entertainment - fn_entertainment )
+
+
+
+    print '\nBusiness Class'
+    print 'True Positive',    
+    print tp_business,
+    print 'False Positive',
+    print fp_business,
+    print 'False Negative',
+    print fn_business,
+    print 'True Negatine',
+    print ( len(class_entertainment) + len(class_business) + len(class_politics) - tp_business - fp_business - fn_business )
+
+
+    print '\nPolitics Class'
+    print 'True Positive',
+    print tp_politics,
+    print 'False Positive',
+    print fp_politics,
+    print 'False Negative',
+    print fn_politics,
+    print 'True Negative',
+    print ( len(class_entertainment) + len(class_business) + len(class_politics) - tp_politics - fp_politics - fn_politics )
+
+    
+    print '\nThe value of F1 is :',
+    print f_aggr   
+    print '\n'
+
+     
+
+    return 
+
+
+def calculate_class(word_list,ttl,exp_class):
+    
+    global probability_entertainment
+    global probability_business
+    global probability_politics
+    
+    global tp_business
+    global tp_politics
+    global tp_entertainment
+
+    global fp_business
+    global fp_politics
+    global fp_entertainment
+
+
+    global fn_business
+    global fn_politics
+    global fn_entertainment
+
+    sum_entertainment=0
+    sum_business=0
+    sum_politics=0
+   
+    for each_term in word_list:
+        
+        numerator=0
+        denominator=0
+        if each_term in term_dict_entertainment:
+           numerator = term_dict_entertainment[each_term]+1
+          
+        else:
+            numerator = 1
+        
+        denominator = total_terms_entertainment_class + len(term_dict_entertainment_frequency)
+        frac = float(numerator) / denominator
+        log_form = float(math.log(frac,2))
+        sum_entertainment+=log_form
+
+
+        numerator=0
+        denominator=0
+        if each_term in term_dict_business:
+            numerator = term_dict_business[each_term]+1
+        else:
+            numerator = 1
+        
+        denominator = total_terms_business_class + len(term_dict_business_frequency)
+        frac = float(numerator) / denominator
+        log_form = float(math.log(frac,2))
+        sum_business+=log_form
+
+
+        numerator=0
+        denominator = 0
+        if each_term in term_dict_politics:
+            numerator = term_dict_politics[each_term]+1
+        else:
+            numerator = 1
+        denominator = total_terms_politics_class + len(term_dict_politics_frequency)
+        frac = float(numerator) / denominator
+        log_form = float(math.log(frac,2))
+        sum_politics+=log_form
+
+    
+
+    val_ent = float(math.log(float(probability_entertainment),2)) + sum_entertainment
+    val_bus = float(math.log(float(probability_business),2)) + sum_business
+    val_pol = float(math.log(float(probability_politics),2)) + sum_politics
+
+
+    max_prob = max(val_ent,val_bus,val_pol)
+   
+    txt=''
+
+    if(exp_class == 0):
+      txt = 'Entertainment'
+    elif(exp_class == 1):
+      txt = 'Business'
+    elif(exp_class == 2):
+      txt = 'Politics'
+
+    tpl=()
+
+            
+
+    if(max_prob == val_ent):
+      tpl = (txt,ttl)
+      class_entertainment.append(tpl)
+      if (exp_class == 0):
+         tp_entertainment+=1
+      if (exp_class == 1):
+         fn_business+=1
+      if (exp_class == 2):
+         fn_politics+=1
+      if (exp_class == 1 or exp_class == 2):
+         fp_entertainment+=1
+       
+    elif(max_prob == val_bus):
+      tpl = (txt,ttl)
+      class_business.append(tpl)
+      if (exp_class == 1):
+         tp_business+=1
+      if (exp_class == 0):
+         fn_entertainment+=1
+      if (exp_class == 2):
+         fn_politics+=1
+      if (exp_class == 0 or exp_class == 2):
+         fp_business+=1
+     
+
+    elif(max_prob == val_pol):
+      tpl = (txt,ttl)
+      class_politics.append(tpl)
+      if (exp_class == 2):
+         tp_politics+=1
+      if (exp_class == 0):
+         fn_entertainment+=1
+      if (exp_class == 1):
+         fn_business+=1
+      if (exp_class == 0 or exp_class == 1):
+         fp_politics+=1
+   
+
+    return
+
+
+def word_preprocessing(word):
+ return re.findall(r"[\w]+", word.lower())
+
+
+def main():
+    
+    naive_bays_classification(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5],sys.argv[6]);
+       
+if __name__ == '__main__':
+  main()
